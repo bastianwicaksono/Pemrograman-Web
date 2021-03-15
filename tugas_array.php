7e82594d71871d70a96018ee26fcd94a3bc9cbb5
bastianwicaksono <61442531+bastianwicaksono@users.noreply.github.com>
2021-03-15 17:22:58 +0700

    Add files via upload

---
+++ tugas_array.php
@@ -0,0 +1,16 @@
+<?php 
+	//Bastian Wicaksono Aji
+	//12191669
+	echo "No 1";
+	echo "<br>";
+	$ikanku = array("Teri","Lele","Bawal","Tuna");
+
+	echo "Ikan $ikanku[0] sering dijumpai di nasi kucing angkringan dan saya konsumsi pas tanggal tua. Kalo tanggal muda sering kuliner $ikanku[1], $ikanku[2], $ikanku[3].";
+
+	echo "<br>";
+	echo "<br>";
+	
+	echo "No 2";
+	echo "<br>";
+	echo date('d-m-Y', strtotime('2017-03-14'));
+?>
